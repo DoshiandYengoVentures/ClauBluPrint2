649eb1fb2cf79549e4a465e1a7842abec5f601bb
DoshiandYengoVentures <doshiandyengoventures@gmail.com>
2026-08-15 15:15:32 -0700

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "pveajzwhknrjoxguwdne";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_g-SzvsauUPo-kF9yssBOag_NVCqvvtX";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
